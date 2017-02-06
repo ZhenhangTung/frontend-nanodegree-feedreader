105b9f51e7df530071719a23e1d4fcb49fad9158
ZhenhangTong <franktung7@gmail.com>
2017-02-06 19:29:21 +0800

    improve comments and bug fix

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -40,7 +40,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -54,25 +54,16 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-
     describe('The menu', function() {
+        /* This test ensures the menu element is
+         * hidden by default.
+         */
         it('ensures the menu element is hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
-
+        /* This test ensures the menu changes
+         * visibility when the menu icon is clicked.
+        */
         it('ensures the menu changes visibility when the menu icon is clicked', function() {
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).not.toBe(true);
@@ -82,35 +73,25 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+    /* This test ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     */
 
     describe('Initial Entries', function() {
         beforeEach(function(done) {
-            loadFeed(0 ,function() {
-                done();
-            });
+            loadFeed(0, done);
         });
 
-        it('there is at least a single .entry element within the .feed container', function(done) {
+        it('there is at least a single .entry element within the .feed container', function() {
             // Find if there is at least 1 .entry elemet in .feed container
-            expect($('.feed').find(".entry").length).toBeGreaterThan(1);
-            done();
+            expect($('.feed').find(".entry").length).toBeGreaterThan(0);
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    /* This test ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     */
         
     describe('New Feed Selection', function() {
         var firstNewsTitleList,
@@ -118,11 +99,11 @@ $(function() {
         beforeEach(function(done) {
             $('.feed').empty();
             loadFeed(0 ,function() {
-                firstNewsTitleList = $('.feed').find("h2");
-            });
-            loadFeed(1 , function() {
-                secondNewsTitleList = $('.feed').find("h2");
-                done();
+                firstNewsTitleListText = $('.feed').find("h2").text();
+                loadFeed(1 , function() {
+                    secondNewsTitleListText = $('.feed').find("h2").text();
+                    done();
+                });
             });
         });
         it('ensures a new feed is loaded and the content actually changes', function(done) {
